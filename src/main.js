9578d2aacc2f358c305240a13c61b54a0c32f8d8
parmery <parmery@sina.com>
2022-03-11 14:24:14 +0800

    添加styles文件夹，新增初始化样式

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from "vue";
+
 import "normalize.css/normalize.css"; // CSS 重置的现代替代方案
+
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import zhCn from "element-plus/es/locale/lang/zh-cn";
+
+import "@/styles/index.scss";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
