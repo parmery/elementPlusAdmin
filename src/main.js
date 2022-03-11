e27e565ebdc0ca51329120c66980ca6264087b46
parmery <parmery@sina.com>
2022-03-11 14:06:19 +0800

    安装并使用element-plus

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import { createApp } from "vue";
 import "normalize.css/normalize.css"; // CSS 重置的现代替代方案
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 const app = createApp(App);
+app.use(ElementPlus, {
+  locale: zhCn,
+});
 app.use(store);
 app.use(router);
 app.mount("#app");
