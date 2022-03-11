5d3fae737ab2213ebc79b2c384609d9116d2d130
parmery <parmery@sina.com>
2022-03-11 15:50:25 +0800

    编写login组件基本样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,168 @@
 <template>
-  <div>login</div>
+  <div class="login-container">
+    <el-form class="login-form">
+      <div class="title-container">
+        <h3 class="title">Login Form</h3>
+      </div>
+
+      <el-form-item>
+        <span class="svg-container">*</span>
+        <el-input v-model="username" />
+      </el-form-item>
+
+      <el-tooltip content="Caps lock is On" placement="right" manual>
+        <el-form-item>
+          <span class="svg-container">*</span>
+          <el-input v-model="password" />
+          <span class="show-pwd">*</span>
+        </el-form-item>
+      </el-tooltip>
+
+      <el-button type="primary" style="width: 100%; margin-bottom: 30px"
+        >Login
+      </el-button>
+
+      <div>
+        <div class="tips">
+          <span>Username : admin</span>
+          <span>Password : any</span>
+        </div>
+        <div class="tips">
+          <span>Username : editor</span>
+          <span>Password : any</span>
+        </div>
+        <el-button class="third-party-button" type="primary"
+          >Or connect with
+        </el-button>
+      </div>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
   name: "login",
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
 };
 </script>
 
-<style scoped></style>
+<style lang="scss">
+$bg: #283443;
+$light_gray: #fff;
+$cursor: #fff;
+
+.login-container {
+  .el-input {
+    display: inline-block;
+    height: 47px;
+    width: 85%;
+
+    input {
+      background: transparent;
+      border: 0;
+      -webkit-appearance: none;
+      border-radius: 0;
+      padding: 12px 5px 12px 15px;
+      color: $light_gray;
+      height: 47px;
+      caret-color: $cursor;
+
+      &:-webkit-autofill {
+        box-shadow: 0 0 0 1000px $bg inset !important;
+        -webkit-text-fill-color: $cursor !important;
+      }
+    }
+  }
+
+  .el-form-item {
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    background: rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    color: red;
+  }
+
+  .el-input__inner {
+    box-shadow: none;
+  }
+}
+</style>
+
+<style lang="scss" scoped>
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
+
+.login-container {
+  width: 100%;
+  height: 100%;
+  background-color: $bg;
+
+  .login-form {
+    position: relative;
+    width: 520px;
+    max-width: 100%;
+    padding: 160px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
+  }
+
+  .tips {
+    font-size: 14px;
+    color: #fff;
+    margin-bottom: 10px;
+
+    span {
+      &:first-of-type {
+        margin-right: 16px;
+      }
+    }
+  }
+
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    width: 30px;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0 auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    top: 7px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
+
+  .third-party-button {
+    position: absolute;
+    right: 0;
+    bottom: 6px;
+  }
+
+  @media only screen and (max-width: 470px) {
+    .third-party-button {
+      display: none;
+    }
+  }
+}
+</style>
