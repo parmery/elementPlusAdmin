c056985d7e6bae15274e1234afc3e786d3ae47a1
parmery <parmery@sina.com>
2022-03-15 11:01:21 +0800

    修改permission

--- src/permission.js
+++ src/permission.js
@@ -8,6 +8,7 @@ const whiteList = ["/login", "/auth-redirect"];
 router.beforeEach(async (to, from, next) => {
   // 判断账号是否登陆
   const hasToken = getToken();
+  console.log("===hasToken=====");
   console.log(hasToken);
 
   if (hasToken) {
@@ -17,7 +18,7 @@ router.beforeEach(async (to, from, next) => {
     } else {
       // 判断用户是否通过getInfo获取了自己的权限角色
       const hasRoles = store.getters.roles && store.getters.roles.length > 0;
-      console.log("============");
+      console.log("====hasRoles=====");
       console.log(hasRoles);
       if (hasRoles) {
         next();
@@ -33,11 +34,14 @@ router.beforeEach(async (to, from, next) => {
             "permission/generateRoutes",
             roles
           );
+          console.log("==accessRoutes==");
+          console.log(accessRoutes);
 
           // // 动态添加可访问路由
-          router.addRoute(accessRoutes);
+          // router.addRoute(accessRoutes);
 
-          next({ ...to, replace: true });
+          // next({ ...to, replace: true });
+          next();
         } catch (error) {
           // 删除令牌并转到登录页面重新登录
           // await store.dispatch("user/resetToken");
@@ -51,7 +55,8 @@ router.beforeEach(async (to, from, next) => {
     if (whiteList.indexOf(to.path) !== -1) {
       next();
     } else {
-      next(`/login?redirect=${to.path}`);
+      // next(`/login?redirect=${to.path}`);
+      next(`/login`);
     }
   }
 });
